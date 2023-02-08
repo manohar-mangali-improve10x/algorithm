6d6a602ca40747feb2d4f8cdcc9590c37aa017bc
Manohar <manohar.mangali@improve10x.com>
2023-02-08 16:04:15 +0530

    created logicalActivity added data

--- app/src/main/java/com/improve10x/algorithm/LogicalActivity.java
+++ app/src/main/java/com/improve10x/algorithm/LogicalActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LogicalActivity extends AppCompatActivity {
-    EditText aTxt;
-    EditText bTxt;
+    SeekBar aSb;
+    SeekBar bSb;
     TextView resultTxt;
     Button checkBtn;
 
@@ -17,36 +19,40 @@ public class LogicalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logical);
-        handleCheckBtn();
         handleIntiViews();
+        handleCheckBtn();
     }
 
+
     private void handleIntiViews() {
-        aTxt = findViewById(R.id.a_txt);
-        bTxt = findViewById(R.id.b_txt);
+        aSb = findViewById(R.id.a_sb);
+        bSb = findViewById(R.id.b_sb);
         resultTxt = findViewById(R.id.result_txt);
         checkBtn = findViewById(R.id.check_btn);
     }
 
     private void handleCheckBtn() {
-        checkBtn.setOnClickListener(view -> {
-            String input1 = aTxt.getText().toString();
-            String input2 =bTxt.getText().toString();
+        checkBtn.setOnClickListener(v -> {
+            int input1 = aSb.getProgress();
+            int input2 = bSb.getProgress();
             String result = sumIsLessThan100(input1,input2);
             resultTxt.setText(result);
-
         });
     }
 
-    private String sumIsLessThan100(String input1, String input2) {
-        int a = Integer.parseInt(input1);
-        int b = Integer.parseInt(input2);
-        boolean result= false;
-        if (a + b <100){
+    private String sumIsLessThan100(int input1, int input2) {
+        boolean result = false;
+        if (input1 + input2<100){
             result = true;
         }
-        return result + " ";
-
+        return result + "";
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        aSb.setProgress(0);
+        bSb.setProgress(0);
+        resultTxt.setText("");
+    }
 }
